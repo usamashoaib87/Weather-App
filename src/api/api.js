2b91c95945e1c94e4a760e4c94afa3304c5700a0
usamashoaib87 <usamashoaib87@gmail.com>
2024-05-28 15:44:12 +0500

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = 'a02a2c2425b5a352bdc932b1e39fec51';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
